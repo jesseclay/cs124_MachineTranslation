61ba8c6c1e8cfdc03b55efa812a4c6405956e4e4
Sathish Nagappan <Sathish_Nagappan@hotmail.com>
2014-02-23 22:38:55 -0800

    direct translation

--- MT.py
+++ MT.py
@@ -1,9 +1,10 @@
 
-import collections
+import collections, nltk
 
 class MachineTranslation:
 
 	def __init__(self):
+		self.translation = []
 		self.dictionary = collections.defaultdict(lambda: 0)
 		dictionaryFile = open("Dictionary.txt", 'r')
 		for translation in dictionaryFile:
@@ -15,4 +16,17 @@ class MachineTranslation:
 		for sentence in sentencesFile:
 			self.sentences.append(sentence.rstrip('\n'))
 
+	def translate(self):
+		for sentence in self.sentences:
+
+			sentenceTranslation = []
+			tokens = nltk.word_tokenize(sentence)
+			for token in tokens:
+				wordTranslation = self.dictionary[token]
+				sentenceTranslation.append(wordTranslation)
+			self.translation.append(" ".join(map(str, sentenceTranslation)))
+
+
 MT = MachineTranslation()
+MT.translate()
+
